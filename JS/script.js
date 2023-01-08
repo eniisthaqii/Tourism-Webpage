af6882ca472d2afd0174cc87910cf660c676b3d1
Enis Thaçi <65370856+eniisthaqii@users.noreply.github.com>
2023-01-08 16:51:58 +0100

    MenuIcon function added

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,12 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+var mic = document.querySelector('.bx-menu');
+
+menu.onclick= () => {
+    menu.classList.toggle('mic');
+    navbar.classList.toggle('open')
+} 
 
 //Image Slider
 
